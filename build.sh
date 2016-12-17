3d1d761b084460198fe61d75a7867d4a9966e717
Infactum <infactum@gmail.com>
2016-12-17 13:58:53 +0500

    tagged build

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -e
 CID=$(docker run -d -v "$(pwd)"/dist:/usr/share/nginx/html:ro nginx:alpine)
 docker build \
     --build-arg WEBHOST="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' "$CID")" \
-    -t infactum/onec_thick_client \
+    -t infactum/onec_thick_client:"$VERSION" \
     thick_client/
 docker stop "$CID" && docker rm "$CID"
